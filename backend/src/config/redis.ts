a4d592c06050a39e995bfcdbc3a7e5e9adb18a92
nafalrust <muhammadnafalzakinrustanto@mail.ugm.ac.id>
2026-09-14 12:57:51 +0700

    feat(queue): add BullMQ + Redis setup

---
+++ backend/src/config/redis.ts
@@ -0,0 +1,6 @@
+import "dotenv/config";
+import IORedis from "ioredis";
+
+export const redisConnection = new IORedis(process.env.REDIS_URL!, {
+  maxRetriesPerRequest: null, // wajib untuk BullMQ
+});
